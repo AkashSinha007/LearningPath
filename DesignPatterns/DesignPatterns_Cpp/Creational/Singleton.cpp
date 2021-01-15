8f881b8c13ecf0e2d400cd6396559625fbd050f8
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-01-15 12:51:36 +0530

    Initial Commit Design Patterns

---
+++ DesignPatterns/DesignPatterns_Cpp/Creational/Singleton.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Singleton
+{
+private:
+    Singleton() { std::cout << "Singleton" << std::endl; }
+    static Singleton *instance_;
+
+public:
+    Singleton(const Singleton &) = delete;
+    Singleton &operator=(const Singleton &) = delete;
+
+    static Singleton *instance()
+    {
+        if (!instance_)
+        {
+            instance_ = new Singleton();
+        }
+        return instance_;
+    }
+
+    void checkSingleton() { std::cout << "Singleton has been created" << std::endl; }
+};
+
+Singleton *Singleton::instance_ = nullptr;
+
+int main(int argc, char *argv[])
+{
+    Singleton *singleton = Singleton::instance();
+    singleton->checkSingleton();
+}
