a3ce2031089fe3e93939a76832097c638833cae4
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-01-17 22:03:22 +0530

    Initial Commit Data Structure

---
+++ Learn_DataStructure/Array/Problem_1_s1.cpp
@@ -0,0 +1,44 @@
+/*
+Reverse the array
+*/
+/*
+https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+*/
+
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<endl;
+}
+
+void reverseArray(int arr[],int start ,int end)
+{
+    while(start<end)
+    {
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6};
+    int n= sizeof(a)/sizeof(a[0]);
+
+    printArray(a, n);
+
+    reverseArray(a,0,n-1);
+
+    printArray(a, n);
+    return 0;
+}
---
+++ Learn_DataStructure/Array/Problem_1_s2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<endl;
+}
+
+void reverseArray(int arr[],int start ,int end)
+{
+    if(start>=end)
+        return;
+
+    int temp=arr[start];
+    arr[start]=arr[end];
+    arr[end]=temp;
+    reverseArray(arr,start+1,end-1);
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6};
+    int n= sizeof(a)/sizeof(a[0]);
+
+    printArray(a, n);
+
+    reverseArray(a,0,n-1);
+
+    printArray(a, n);
+    return 0;
+}
