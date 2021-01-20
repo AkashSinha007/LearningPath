d5b42812ae8a7390f8aa38d33d0fd3be1c52876f
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-01-20 20:42:57 +0530

    RabbitMQ Code completed

---
+++ Microservices_Tutorial/MicroservicesApp/src/Basket/BasketAPI/Mapping/BasketMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BasketAPI.Entities;
+using EventBusRabbitMQ.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketAPI.Mapping
+{
+    public class BasketMapping:Profile
+    {
+        public BasketMapping()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
--- Microservices_Tutorial/MicroservicesApp/src/Basket/BasketAPI/Startup.cs
+++ Microservices_Tutorial/MicroservicesApp/src/Basket/BasketAPI/Startup.cs
@@ -1,7 +1,10 @@
+using AutoMapper;
 using BasketAPI.Data;
 using BasketAPI.Data.Interfaces;
 using BasketAPI.Repositories;
 using BasketAPI.Repositories.Interfaces;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Producer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
@@ -30,6 +34,8 @@ namespace BasketAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddSingleton<ConnectionMultiplexer>(sp =>
             {
                 var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
@@ -38,13 +44,35 @@ namespace BasketAPI
 
             services.AddTransient<IBasketContext, BasketContext>();
             services.AddTransient<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddSwaggerGen(c=>
             {
                 c.SwaggerDoc("v1",new OpenApiInfo { Title="Basket API",Version="v1"});
             });
 
-            services.AddControllers();
+            services.AddSingleton<IRabbitMQConnection>(sp =>
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Configuration["EventBus:HostName"]
+                };
+                if (!string.IsNullOrEmpty(Configuration["EventBus:UserName"]))
+                {
+                    factory.UserName = Configuration["EventBus:UserName"];
+                }
+
+                if (!string.IsNullOrEmpty(Configuration["EventBus:Password"]))
+                {
+                    factory.Password = Configuration["EventBus:Password"];
+                }
+
+                return new RabbitMQConnection(factory);
+
+            });
+
+            services.AddSingleton<EventBusRabbitMQProducer>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
